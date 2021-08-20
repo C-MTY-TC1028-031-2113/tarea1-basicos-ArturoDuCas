5f3544c69b6eaa9b505249e13b0d88b30dee951a
ArturoDuCas <a00833516@itesm.mx>
2021-08-19 21:39:34 -0500

    aaa

--- assignments/04PEMDAS/src/exercise.py
+++ assignments/04PEMDAS/src/exercise.py
@@ -1,8 +1,11 @@
 def main():
     from math import sqrt
-    pass
-    
-   
+    print (str(round((2*(3/4)+4*(2/3)-3*(1/5)+5*(1/2),4))))
+    print (str(round((2*35+4*sqrt(36**3)-6*49),4)))
+    print (str(round((4**3+2*5**2)/(4*4),4)))
+    print (str(round((2*(4+5)**2+4*(4-5)**2)/(4*5**2),4)))
+    print (str(round((sqrt((4+5)**2+2**(4+5))/(2*4+2*5)**2),4)))
+
 
 
 
